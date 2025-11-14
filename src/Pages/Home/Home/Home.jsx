2912379b22f60f70f464b4f13185dba93f92a1af
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-11-15 05:04:58 +0600

    added team

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -4,6 +4,7 @@ import Company from '../Company/Company';
 import Service from '../Services/Service';
 import CaseStudies from '../CaseStudies/CaseStudies';
 import WorkingProcess from '../WorkingProcess/WorkingProcess';
+import Team from '../Team/Team';
 
 const Home = () => {
     return (
@@ -13,6 +14,7 @@ const Home = () => {
            <Service></Service>
            <CaseStudies></CaseStudies>
            <WorkingProcess></WorkingProcess>
+           <Team></Team>
         </div>
     );
 };
