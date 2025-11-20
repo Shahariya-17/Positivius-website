c814997ebc43ad5c7ee2290fa4b914b654a13b8d
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-11-20 12:09:09 +0600

    added responsive footer

--- src/Pages/Home/Team/Team.jsx
+++ src/Pages/Home/Team/Team.jsx
@@ -4,7 +4,7 @@ const Team = () => {
     return (
         <div className='border-2 px-10 py-8 mt-20 mb-20'>
             <h1 className='text-3xl font-bold text-center'>This is team</h1>
-            <p>Lorem ipsum dolor sit amet.</p>
+            <p className='text-center'>Lorem ipsum dolor sit amet.</p>
         </div>
     );
 };
