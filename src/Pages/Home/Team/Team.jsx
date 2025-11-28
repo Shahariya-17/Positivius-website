75caffe57bbccce07451bf0b3c1756584fa50a71
Shahariya-17 <shahariyarrakib780@gmail.com>
2025-11-28 14:57:40 +0600

    added responsive navbar

--- src/Pages/Home/Team/Team.jsx
+++ src/Pages/Home/Team/Team.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
 const Team = () => {
-    return (
-        <div className='text-center'>
-            <h1 className="text-4xl">This is team</h1>
-        </div>
-    );
+  return (
+   
+  );
 };
 
 export default Team;
